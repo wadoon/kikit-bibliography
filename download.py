f28151d691f96a15f0961b635dbaeaba71c10c2a
Alexander Weigl <weigl@kit.edu>
2024-08-09 00:28:27 +0200

    add dblp download

--- download.py
+++ download.py
@@ -30,7 +30,7 @@ def _fetch(name, kit_id, dblp_id):
     print(f"Download data for {name}")
 
     print(f"\tDBLP", end=" ")
-    #download_to(f"https://dblp.uni-trier.de/pid/{dblp_id}.xml", TMP/f"{name}.xml")
+    download_to(f"https://dblp.uni-trier.de/pid/{dblp_id}.xml", TMP/f"{name}.xml")
     print(f"ok\n\tKIT all", end=" ")
     all_pubs=f"https://publikationen.bibliothek.kit.edu/publikationslisten/get.php?referencing=all&external_publications=all&lang=de&format=csl_json&style=kit-3lines-title_b-authors-other&consider_suborganizations=true&contributors=%5B%5B%5B%5D%2C%5B%22{kit_id}%22%5D%5D%5D&year=2021-2024"
 
