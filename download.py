c2a54bd372f3ce140e200fe3acfc835e502f9b32
Alexander Weigl <wadoon@users.noreply.github.com>
2026-03-16 22:04:39 +0000

    use dblp.org instead of dblp.uni-trier.de

--- download.py
+++ download.py
@@ -47,7 +47,7 @@ KIT_TAGGING="kit_tagging=11784.105OR11825.105OR11785.105OR11786.105OR11829.105OR
 def _fetch(name, kit_id, dblp_id):
     print(f"Download data for {name}")
 
-    download_to(f"https://dblp.uni-trier.de/pid/{dblp_id}.xml", TMP/f"{name}.xml",
+    download_to(f"https://dblp.org/pid/{dblp_id}.xml", TMP/f"{name}.xml",
                 desc = "DBLP")
 
     all_pubs=f"https://publikationen.bibliothek.kit.edu/publikationslisten/get.php?referencing=all&external_publications=all&lang=de&format=csl_json&style=kit-3lines-title_b-authors-other&consider_suborganizations=true&contributors=%5B%5B%5B%221000.105%22%5D%2C%5B%22{kit_id}%22%5D%5D%5D&year=2021-"
